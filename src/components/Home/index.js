f71bb107e4e0e70affdd554a5ef06e591dbeb4d2
rreplogle <rreplogle@zotecpartners.com>
2022-10-27 08:17:14 -0400

    add feature components

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,3 +1,21 @@
-const Home = () => 'Hello'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+
+const Home = () => {
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>Hello</h1>
+        </div>
+        <br />
+        <br />
+      </div>
+    </>
+  )
+}
 
 export default Home
